c24b0f3dc92dbec99fa1c8cdde15ae9ffb71254b
Riday <rdmondal100@gmail.com>
2024-10-17 23:04:03 +0600

    fix the issue with the origin dicleratino

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 
 app.use(cors({
-  origin: [process.env.CORS_ORIGIN],
+  origin: process.env.CORS_ORIGIN,
   credentials: true
 }))
 
